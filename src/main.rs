0a460cf99f4e4e4867042addcfcd47b8af5189c8
Unikii <97149134+DaddyUnikii@users.noreply.github.com>
2025-10-03 08:43:15 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,3 +3,9 @@ fn main() {
     println!("📚 Crypto Glossary");
     println!("Istilah crypto untuk pemula");
 }
+mod commands;
+
+fn main() {
+    println!("📚 Crypto Glossary CLI");
+    commands::check_price();
+}
