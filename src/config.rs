69ac8cd68caf24ba4d401b29f08f5a98dd99d6cd
Unikii <97149134+DaddyUnikii@users.noreply.github.com>
2025-10-05 09:54:18 +0700

    Create config.rs

---
+++ src/config.rs
@@ -0,0 +1,8 @@
+// Simulasi baca konfigurasi dari config.toml
+pub fn get_config() -> String {
+    "Baca konfigurasi dari config.toml (simulasi)".to_string()
+}
+
+pub fn get_default_coin() -> String {
+    "BTC".to_string()
+}
